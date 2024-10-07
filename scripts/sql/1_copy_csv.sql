afdeea07913275947a21fe3b547657661a7528dd
Thai Villaluna <thaivillaluna@gmail.com>
2024-10-07 14:42:25 -0700

    adjust sql scripts

--- scripts/sql/1_copy_csv.sql
+++ scripts/sql/1_copy_csv.sql
@@ -1,3 +1,5 @@
-.import --csv --skip 1 ../preprocessed/player_db.csv player
-.import --csv --skip 1 ../preprocessed/match_stat_db.csv match_stat
-.import --csv --skip 1 ../preprocessed/player_stat_db.csv player_stat
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+.import --csv --skip 1 $SCRIPT_DIR/../../data/preprocessed/player_db.csv player
+.import --csv --skip 1 $SCRIPT_DIR/../../data/preprocessed/match_stat_db.csv match_stat
+.import --csv --skip 1 $SCRIPT_DIR/../../data/preprocessed/player_stat_db.csv player_stat
