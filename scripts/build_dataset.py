621e3a2d8fcde98d6e658556f724d4a3af39ca4e
Thai Villaluna <thaivillaluna@gmail.com>
2024-10-09 11:08:04 -0700

    commit to database after every month

--- scripts/build_dataset.py
+++ scripts/build_dataset.py
@@ -10,52 +10,53 @@ def main():
     
     con, cur = connection.connect()
     for year in range(2014, 2024):
-        check_year = cur.execute(f"""
-                                 SELECT date
-                                 FROM dataset
-                                 WHERE strftime('%Y', date) = '{year}'
-                                 """)
-        if check_year.fetchone() is not None:
-            print(f"Skipping {year} because already in dataset")
-            continue
+        for month in range(1, 13):
+            check_month = cur.execute(f"""
+                                    SELECT date
+                                    FROM dataset
+                                    WHERE strftime('%Y/%m', date) = '{year}/{month:02}'
+                                    """)
+            if check_month.fetchone() is not None:
+                print(f"Skipping {year}-{month} because already in dataset")
+                continue
 
-        match_info = cur.execute(f"""
-                        SELECT match_stat.match_key, player_stat.player_key, date,
-                        position, player_stat.kills
-                        FROM match_stat
-                        INNER JOIN player_stat 
-                            ON match_stat.match_key = player_stat.match_key
-                        WHERE strftime('%Y', date) = '{year}'
-                        ORDER BY date ASC;
-                        """)
-    
+            match_info = cur.execute(f"""
+                            SELECT match_stat.match_key, player_stat.player_key, date,
+                            position, player_stat.kills
+                            FROM match_stat
+                            INNER JOIN player_stat 
+                                ON match_stat.match_key = player_stat.match_key
+                            WHERE strftime('%Y', date) = '{year}'
+                            ORDER BY date ASC;
+                            """)
         
-        for match_key, player_key, date, position, kills in match_info.fetchall():
-            if match_key.endswith("_Red"):
-                opponent_match_key = match_key[:-4] + "_Blue"
-            elif match_key.endswith("_Blue"):
-                opponent_match_key = match_key[:-5] + "_Red"
-            else:
-                raise Exception("Match key doesn't end with Blue or Red: %s", match_key)
             
-            opponent_info = cur.execute(f"""
-                            SELECT player_key
-                            FROM player_stat
-                            WHERE position = '{position}'
-                                and match_key = '{opponent_match_key}';
-                            """) 
-            opponent_key = opponent_info.fetchone()[0]
-            
-            player_stats = queries.calculate_averages(player_key, date=date, con=con)
-            opponent_stats = queries.calculate_averages(opponent_key, date=date, con=con)
-        
-            data_point = (match_key, position, date, kills) + player_stats + opponent_stats
-            
-            print(f"Adding data point for {match_key} {position} on {date}")
-            cur.execute(query_helpers.INSERT_DATASET_SQL, data_point)
-        
-        con.commit()
+            for match_key, player_key, date, position, kills in match_info.fetchall():
+                if match_key.endswith("_Red"):
+                    opponent_match_key = match_key[:-4] + "_Blue"
+                elif match_key.endswith("_Blue"):
+                    opponent_match_key = match_key[:-5] + "_Red"
+                else:
+                    raise Exception("Match key doesn't end with Blue or Red: %s", match_key)
                 
+                opponent_info = cur.execute(f"""
+                                SELECT player_key
+                                FROM player_stat
+                                WHERE position = '{position}'
+                                    and match_key = '{opponent_match_key}';
+                                """) 
+                opponent_key = opponent_info.fetchone()[0]
+                
+                player_stats = queries.calculate_averages(player_key, date=date, con=con)
+                opponent_stats = queries.calculate_averages(opponent_key, date=date, con=con)
+            
+                data_point = (match_key, position, date, kills) + player_stats + opponent_stats
+                
+                print(f"Adding data point for {match_key} {position} on {date}")
+                cur.execute(query_helpers.INSERT_DATASET_SQL, data_point)
+            
+            con.commit()
+                    
 
 
 if __name__=="__main__":
